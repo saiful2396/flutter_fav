cd734b45e913096f1b5152a039e530ee7e6b802b
Saiful Islam <saifulislampp23@gmail.com>
2021-01-08 21:19:21 +0600

    update

--- lib/screen_view/fav_page.dart
+++ lib/screen_view/fav_page.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fav/product_provider/product_provider.dart';
+import 'package:flutter_fav/widget/product_item.dart';
+import 'package:provider/provider.dart';
 
 class FavPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //final favIem = Provider.of<ProductProvider>(context).favorites;
+    final favIem = Provider.of<ProductProvider>(context).favorites;
     return Scaffold(
       appBar: AppBar(
         title: Text('Favorite Page'),
       ),
-      body: Center(child: Text('Favorite'),),
-      // body: GridView.builder(
-      //     itemCount: favIem.length,
-      //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      //       crossAxisCount: 2,
-      //       childAspectRatio: 3 / 2,
-      //     ),
-      //     itemBuilder: (ctx, i) {
-      //       return ChangeNotifierProvider.value(
-      //           value: favIem[i], child: favIem[i].id.isEmpty ? Container() : ProductItem());
-      //     }),
+      body: GridView.builder(
+          itemCount: favIem.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+          ),
+          itemBuilder: (ctx, i) {
+            return ChangeNotifierProvider.value(
+                value: favIem[i], child: favIem[i].id.isEmpty ? Container() : ProductItem());
+          }),
     );
   }
 }
--- lib/widget/product_item.dart
+++ lib/widget/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fav/product_provider/product.dart';
+import 'package:flutter_fav/product_provider/product_provider.dart';
 import 'package:flutter_fav/screen_view/product_details.dart';
 import 'package:provider/provider.dart';
 
@@ -7,6 +8,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context);
+    final productProvider = Provider.of<ProductProvider>(context);
     return GestureDetector(
       onTap: () {
         Navigator.of(context)
@@ -19,12 +21,16 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black54,
-          leading:  IconButton(
-            icon: Icon(product.isFavourite
-                ? Icons.favorite
-                : Icons.favorite_border),
+          leading: IconButton(
+            icon: Icon(
+                product.isFavourite || productProvider.isFavorite(product)
+                    ? Icons.favorite
+                    : Icons.favorite_border),
             onPressed: () {
               product.toggleFavorite();
+              productProvider.isFavorite(product)
+                  ? productProvider.removeFavorite(product)
+                  : productProvider.addFavorite(product);
             },
             color: Theme.of(context).accentColor,
           ),
